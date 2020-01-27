a48e0acc8406a16865c6526c4091a5bf08cb9e13
VarshiniMoorthy <50829652+VarshiniMoorthy@users.noreply.github.com>
2020-01-27 11:59:40 +0530

    Add files via upload

---
+++ ArrayList/ArrayList/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ArrayList
+{
+    class Class1
+    {
+        static string adminName = ConfigurationManager.appSetting["username"].ToString();
+        static string adminPassword = ConfigurationManager.appSetting["123"].ToString();
+
+    }
+}
